936e7dab8d49adfe032e7571f8592b02d4b66340
Armaghan Saqib <ar.saqib@gmail.com>
2025-06-07 06:09:11 +0500

    perltidy

--- am.pl
+++ am.pl
@@ -16,17 +16,17 @@
 #######################################################################
 
 # setup defaults, DO NOT CHANGE
-$userspath = "users";
-$spool = "spool";
-$templates = "templates";
-$images = "images";
+$userspath  = "users";
+$spool      = "spool";
+$templates  = "templates";
+$images     = "images";
 $memberfile = "users/members";
-$sendmail = "| /usr/sbin/sendmail -t";
-$latex = 0;
+$sendmail   = "| /usr/sbin/sendmail -t";
+$latex      = 0;
 ########## end ###########################################
 
 BEGIN {
-  push @INC, '.';
+    push @INC, '.';
 }
 
 $| = 1;
@@ -39,11 +39,12 @@ $form = new Form $userspath;
 
 # name of this script
 $0 =~ tr/\\/\//;
-$pos = rindex $0, '/';
-$script = substr($0, $pos + 1);
+$pos    = rindex $0, '/';
+$script = substr( $0, $pos + 1 );
 
 # we use $script for the language module
 $form->{script} = $script;
+
 # strip .pl for translation files
 $script =~ s/\.pl//;
 
@@ -56,16 +57,16 @@ $form->{login} =~ s/(\.\.|\/|\\|\x00)//g;
 eval { require("$userspath/$form->{login}.conf"); };
 
 if ($@) {
-  $locale = new Locale "$language", "$script";
-  
-  $form->{callback} = "";
-  $msg1 = $locale->text('You are logged out!');
-  $msg2 = $locale->text('Login');
-  $form->redirect("$msg1 <p><a href=login.pl target=_top>$msg2</a>");
-  exit;
+    $locale = new Locale "$language", "$script";
+
+    $form->{callback} = "";
+    $msg1             = $locale->text('You are logged out!');
+    $msg2             = $locale->text('Login');
+    $form->redirect("$msg1 <p><a href=login.pl target=_top>$msg2</a>");
+    exit;
 }
 
-$form->db_init(\%myconfig);
+$form->db_init( \%myconfig );
 
 # locale messages
 $locale = new Locale "$myconfig{countrycode}", "$script";
@@ -73,156 +74,161 @@ $locale = new Locale "$myconfig{countrycode}", "$script";
 $form->{charset} = $myconfig{charset};
 
 # send warnings to browser
-$SIG{__WARN__} = sub { eval { $form->info($_[0]); } };
+$SIG{__WARN__} = sub {
+    eval { $form->info( $_[0] ); }
+};
 
 # send errors to browser
-$SIG{__DIE__} = sub { eval { $form->error($_[0]); } };
+$SIG{__DIE__} = sub {
+    eval { $form->error( $_[0] ); }
+};
 
 $myconfig{dbpasswd} = unpack 'u', $myconfig{dbpasswd};
-map { $form->{$_} = $myconfig{$_} } qw(stylesheet timeout) unless ($form->{type} eq 'preferences');
+map { $form->{$_} = $myconfig{$_} } qw(stylesheet timeout) unless ( $form->{type} eq 'preferences' );
 
 $form->{path} =~ s/\.\.//g;
-if ($form->{path} !~ /^bin\//) {
-  $form->error($locale->text('Invalid path!')."\n");
+if ( $form->{path} !~ /^bin\// ) {
+    $form->error( $locale->text('Invalid path!') . "\n" );
 }
 
 # global lock out
-if (-f "$userspath/nologin.LCK") {
-  if (-s "$userspath/nologin.LCK") {
-    open(FH, "$userspath/nologin.LCK");
-    $message = <FH>;
-    close(FH);
-    $form->error($message);
-  }
-  $form->error($locale->text('System currently down for maintenance!'));
+if ( -f "$userspath/nologin.LCK" ) {
+    if ( -s "$userspath/nologin.LCK" ) {
+        open( FH, "$userspath/nologin.LCK" );
+        $message = <FH>;
+        close(FH);
+        $form->error($message);
+    }
+    $form->error( $locale->text('System currently down for maintenance!') );
 }
 
 # dataset lock out
-if (-f "$userspath/$myconfig{dbname}.LCK" && $form->{login} ne "admin\@$myconfig{dbname}") {
-  if (-s "$userspath/$myconfig{dbname}.LCK") {
-    open(FH, "$userspath/$myconfig{dbname}.LCK");
-    $message = <FH>;
-    close(FH);
-    $form->error($message);
-  }
-  $form->error($locale->text('Dataset currently down for maintenance!'));
-} 
+if ( -f "$userspath/$myconfig{dbname}.LCK" && $form->{login} ne "admin\@$myconfig{dbname}" ) {
+    if ( -s "$userspath/$myconfig{dbname}.LCK" ) {
+        open( FH, "$userspath/$myconfig{dbname}.LCK" );
+        $message = <FH>;
+        close(FH);
+        $form->error($message);
+    }
+    $form->error( $locale->text('Dataset currently down for maintenance!') );
+}
 
 # pull in the main code
 require "$form->{path}/$form->{script}";
 
 # customized scripts
-if (-f "$form->{path}/custom/$form->{script}") {
-  eval { require "$form->{path}/custom/$form->{script}"; };
+if ( -f "$form->{path}/custom/$form->{script}" ) {
+    eval { require "$form->{path}/custom/$form->{script}"; };
 }
 
 # customized scripts for login
-if (-f "$form->{path}/custom/$form->{login}/$form->{script}") {
-  eval { require "$form->{path}/custom/$form->{login}/$form->{script}"; };
+if ( -f "$form->{path}/custom/$form->{login}/$form->{script}" ) {
+    eval { require "$form->{path}/custom/$form->{login}/$form->{script}"; };
 }
 
+if ( $form->{action} ) {
 
-if ($form->{action}) {
-  # window title bar, user info
-  $form->{titlebar} = "SQL-Ledger - $myconfig{name} - $myconfig{dbname}";
+    # window title bar, user info
+    $form->{titlebar} = "SQL-Ledger - $myconfig{name} - $myconfig{dbname}";
 
-  &check_password;
+    &check_password;
 
-  if (substr($form->{action}, 0, 1) =~ /( |\.)/) {
-    &{ $form->{nextsub} };
-  } else {
-    &{ $locale->findsub($form->{action}) };
-  }
+    if ( substr( $form->{action}, 0, 1 ) =~ /( |\.)/ ) {
+        &{ $form->{nextsub} };
+    } else {
+        &{ $locale->findsub( $form->{action} ) };
+    }
 } else {
-  $form->error($locale->text('action= not defined!'));
+    $form->error( $locale->text('action= not defined!') );
 }
 
 1;
-# end
 
+# end
 
 sub check_password {
 
-  if ($myconfig{password}) {
+    if ( $myconfig{password} ) {
 
-    require "$form->{path}/pw.pl";
+        require "$form->{path}/pw.pl";
 
-    if ($form->{password}) {
-      if ((crypt $form->{password}, substr($form->{login}, 0, 2)) ne $myconfig{password}) {
-	if ($ENV{HTTP_USER_AGENT}) {
-	  &getpassword;
-	} else {
-	  $form->error($locale->text('Access Denied!'));
-	}
-	exit;
-      } else {
-        # password checked out, create session
-	if ($ENV{HTTP_USER_AGENT}) {
-	  # create new session
-	  use SL::User;
-	  $user = new User $memberfile, $form->{login};
-	  $user->{password} = $form->{password};
-	  $user->create_config("$userspath/$form->{login}.conf");
-	  $form->{sessioncookie} = $user->{sessioncookie};
-	}
-      }
-    } else {
+        if ( $form->{password} ) {
+            if ( ( crypt $form->{password}, substr( $form->{login}, 0, 2 ) ) ne $myconfig{password} ) {
+                if ( $ENV{HTTP_USER_AGENT} ) {
+                    &getpassword;
+                } else {
+                    $form->error( $locale->text('Access Denied!') );
+                }
+                exit;
+            } else {
 
-      if ($ENV{HTTP_USER_AGENT}) {
-	$ENV{HTTP_COOKIE} =~ s/;\s*/;/g;
-	@cookies = split /;/, $ENV{HTTP_COOKIE};
-	%cookie = ();
-	foreach (@cookies) {
-	  ($name,$value) = split /=/, $_, 2;
-	  $cookie{$name} = $value;
-	}
+                # password checked out, create session
+                if ( $ENV{HTTP_USER_AGENT} ) {
 
-        $login = $form->{login};
-        $login =~ s/(\@| )/_/g;
+                    # create new session
+                    use SL::User;
+                    $user = new User $memberfile, $form->{login};
+                    $user->{password} = $form->{password};
+                    $user->create_config("$userspath/$form->{login}.conf");
+                    $form->{sessioncookie} = $user->{sessioncookie};
+                }
+            }
+        } else {
 
-	if ($cookie{"SL-$login"}) {
+            if ( $ENV{HTTP_USER_AGENT} ) {
+                $ENV{HTTP_COOKIE} =~ s/;\s*/;/g;
+                @cookies = split /;/, $ENV{HTTP_COOKIE};
+                %cookie  = ();
+                foreach (@cookies) {
+                    ( $name, $value ) = split /=/, $_, 2;
+                    $cookie{$name} = $value;
+                }
 
-	  $form->{sessioncookie} = $cookie{"SL-$login"};
+                $login = $form->{login};
+                $login =~ s/(\@| )/_/g;
 
-	  $s = "";
-	  %ndx = ();
-	  $l = length $form->{sessioncookie};
+                if ( $cookie{"SL-$login"} ) {
 
-	  for $i (0 .. $l - 1) {
-	    $j = substr($myconfig{sessionkey}, $i * 2, 2);
-	    $ndx{$j} = substr($cookie{"SL-$login"}, $i, 1);
-	  }
+                    $form->{sessioncookie} = $cookie{"SL-$login"};
 
-	  for (sort keys %ndx) {
-	    $s .= $ndx{$_};
-	  }
+                    $s   = "";
+                    %ndx = ();
+                    $l   = length $form->{sessioncookie};
 
-	  $l = length $form->{login};
-	  $login = substr($s, 0, $l);
-	  $password = substr($s, $l, (length $s) - ($l + 10));
+                    for $i ( 0 .. $l - 1 ) {
+                        $j = substr( $myconfig{sessionkey}, $i * 2, 2 );
+                        $ndx{$j} = substr( $cookie{"SL-$login"}, $i, 1 );
+                    }
 
-          $flogin = $form->{login};
-          $flogin =~ s/(\@| )/_/g;
+                    for ( sort keys %ndx ) {
+                        $s .= $ndx{$_};
+                    }
 
-          # validate cookie
-	  if (($login ne $flogin) || ($myconfig{password} ne crypt $password, substr($form->{login}, 0, 2))) {
-	    &getpassword(1);
-	    exit;
-	  }
+                    $l        = length $form->{login};
+                    $login    = substr( $s, 0, $l );
+                    $password = substr( $s, $l, ( length $s ) - ( $l + 10 ) );
 
-	} else {
+                    $flogin = $form->{login};
+                    $flogin =~ s/(\@| )/_/g;
 
-	  if ($form->{action} ne 'display') {
-	    &getpassword(1);
-	    exit;
-	  }
+                    # validate cookie
+                    if ( ( $login ne $flogin ) || ( $myconfig{password} ne crypt $password, substr( $form->{login}, 0, 2 ) ) ) {
+                        &getpassword(1);
+                        exit;
+                    }
 
-	}
-      } else {
-	exit;
-      }
+                } else {
+
+                    if ( $form->{action} ne 'display' ) {
+                        &getpassword(1);
+                        exit;
+                    }
+
+                }
+            } else {
+                exit;
+            }
+        }
     }
-  }
 }
-
 
